6da7b311639a60c83d24c2c12d1fb15f0efd1c4a
Xolane Shabalala <143074915+Xolane3@users.noreply.github.com>
2024-09-09 01:19:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("active");
+}
+
+function applyNow() {
+    alert("Thank you for your interest! Please send your application to careers@tech.com");
+}
